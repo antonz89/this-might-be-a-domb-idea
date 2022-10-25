85144caf272a46cc266e7995c16f462570019798
Anton Zakoverya <antonzakoverya@macbook-pro.mynetworksettings.com>
2022-10-25 08:59:49 -0400

    uploading

---
+++ main.js
@@ -0,0 +1,29 @@
+let p = document.querySelector("p");
+p.style.color = "lightblue";
+p.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+let heading = document.querySelector("h1");
+heading.style.fontSize = "48px";
+
+let item13 = document.querySelector("#item-13");
+item13.style.opacity = '0.5'
+
+let item3 = document.querySelector("#item-3");
+item3.innerText = "I say, 'Hi!'"
+
+let image = document.querySelector('img');
+image.src = "https://images.freeimages.com/images/previews/01a/technology-background-1632715.jpg"
+image.style.height = "300px"
+
+let image1 = document.querySelector("#img01");
+image1.src = "https://images.freeimages.com/images/previews/01a/technology-background-1632715.jpg"
+image1.style.height = "300px"
+
+let item16 = document.querySelector("#item-16");
+item16.className = "item";
+item16.innerText = "Won't get fooled again."
+
+
+
+
+
